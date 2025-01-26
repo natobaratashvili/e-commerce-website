07929b3c904ea6700b7da4c262aa1c5ef2a29462
natobaratashvili <natobaratashvili09@gmail.com>
2025-01-26 16:20:32 +0400

    The search bar on the header is only shown on  the home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FilterService } from '../../filter.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +11,17 @@ import { FilterService } from '../../filter.service';
 })
 export class HeaderComponent {
   filterText: string = '';
+  showSearchBar: boolean = false;
   
-  constructor(private filterService: FilterService) { }
+  
+  constructor(private filterService: FilterService, private router: Router) { }
+
+  ngOnInit() {
+    const homeUrl = '/';
+    this.router.events.subscribe(() => {
+      this.showSearchBar = this.router.url === homeUrl; 
+    });
+  }
 
   onFilterChange() {
     this.filterService.setFilter(this.filterText);
